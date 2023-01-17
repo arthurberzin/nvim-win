5b8659aae262bd7af7d98f19b6e9424df094062b
berart <berart@protonmail.com>
2023-01-17 20:35:43 +0200

    Mason border

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -52,7 +52,7 @@ local DEFAULT_SETTINGS = {
         check_outdated_packages_on_open = true,
 
         -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
-        border = "none",
+        border = "rounded",
 
         icons = {
             -- The list icon to use for installed packages.
